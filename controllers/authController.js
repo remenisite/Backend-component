fc310e501adc62820e7f54f5f8063a3a110fabc5
remenisite <antorremeni@gmail.com>
2026-05-15 22:28:53 +0600

    update folders

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,60 +1,103 @@
-const { generateAccessToken, generateRefreshToken } = require("../helpers/utils");
-const userSchema = require("../models/userSchema");
+const {
+  generateAccTkn,
+  generateRefTkn,
+  verifyToken,
+} = require("../services/helpers");
+const { responseHandler } = require("../services/responseHandler");
 
-const signUp = async (req, res) => {
-  const { fullName, email, password } = req.body;
+// ---------------- Signup ----------------
+const signup = async (req, res) => {
   try {
-    if (!fullName)
-      return res.status(400).send({ message: "FullName is required." });
-    if (!email) return res.status(400).send({ message: "Email is required." });
-    if (!password)
-      return res.status(400).send({ message: "Password is required." });
+    const { fullName, email, password, phone, address } = req.body;
 
-    const existEmail = await userSchema.findOne({ email });
-    if (existEmail)
-      return res.status(400).send({ message: "This email already exist." });
-    const user = userSchema.create({
+    if (!email) return responseHandler.error(res, 400, "Email is required");
+    if (!isvelidEmail(email))
+      return responseHandler.error(res, 400, "Invalid email");
+    if (!password)
+      return responseHandler.error(res, 400, "Password is required");
+    if (!isvalidPassword(password))
+      return responseHandler.error(res, 400, "Invalid password");
+
+    const existingUser = await userSchema.findOne({ email });
+    if (existingUser)
+      return responseHandler.error(
+        res,
+        400,
+        "User already exists with this email",
+      );
+
+    const otp = generateOTP();
+    const user = new userSchema({
       fullName,
       email,
       password,
+      phone,
+      address,
+      otp,
+      otpExpires: Date.now() + 2 * 60 * 1000, // 2 mins
     });
-    res.status(200).send({ message: "Registration successfully" });
+
+    await user.save();
+
+    await sendEmail({
+      email,
+      subject: "Verify Your Email",
+      otp,
+      template: emailTemplate,
+    });
+
+    responseHandler.success(
+      res,
+      201,
+      "Signup successful. Please verify your email.",
+    );
   } catch (error) {
     console.log(error);
-    res.status(500).send({ message: "Internal Server Error." });
+    responseHandler.error(res, 500, "Internal Server Error");
   }
 };
 
-const cookie_config = {
-  httpOnly: false, // Not accessible by client-side JS
-  secure: false, // Only sent over HTTPS
-  // sameSite: 'Strict' // Only send for same-site requests
-};
-
-const signIn = async (req, res) => {
-  const { email, password } = req.body;
+// ---------------- Signin ----------------
+const signin = async (req, res) => {
   try {
-    const userData = await userSchema.findOne({ email }).select("+password");
+    const { email, password } = req.body;
 
-    if (!userData)
-      return res.status(400).send({ message: "Invalid crediential" });
-    if (userData.isVerified === false)
-      return res.status(400).send({ message: "Email is not verified" });
+    if (!email) return responseHandler.error(res, 400, "Email is required");
+    if (!isvelidEmail(email))
+      return responseHandler.error(res, 400, "Invalid email");
+    if (!password)
+      return responseHandler.error(res, 400, "Password is required");
+    if (!isvalidPassword(password))
+      return responseHandler.error(res, 400, "Invalid password");
 
-    const matchPassword = await userData.comparePassword(password);
-    if (!matchPassword)
-      return res.status(400).send({ message: "Invalid crediential" });
-    const accToken = generateAccessToken(userData);
-    const refToken = generateRefreshToken(userData);
-    res
-      .status(200)
-      .cookie("acc_tkn", accToken, cookie_config)
-      .cookie("ref_tkn", refToken, cookie_config)
-      .send({ message: "Login Successfylly" });
+    const user = await userSchema.findOne({ email });
+    if (!user) return responseHandler.error(res, 400, "User not found");
+
+    const matchPass = await user.comparePassword(password);
+    if (!matchPass)
+      return responseHandler.error(res, 400, "Incorrect password");
+    if (!user.isVerified)
+      return responseHandler.error(res, 400, "Email is not verified");
+
+    const AccTkn = generateAccTkn(user);
+    const RefTkn = generateRefTkn(user);
+
+    res.cookie("R-XS-Token", AccTkn, {
+      httpOnly: true,
+      secure: false,
+      maxAge: 3600000,
+    });
+    res.cookie("R-RF-Token", RefTkn, {
+      httpOnly: true,
+      secure: false,
+      maxAge: 3600000,
+    });
+
+    responseHandler.success(res, 200, "Signin successful");
   } catch (error) {
     console.log(error);
-    res.status(500).send({ message: "Internal Server Error." });
+    responseHandler.error(res, 500, "Internal Server Error");
   }
 };
 
-module.exports = { signUp, signIn };
+module.exports = { signup, signin };
