f85dcd96c53638085b519a64bdfdd42e23126631
remenisite <antorremeni@gmail.com>
2026-05-19 22:23:36 +0600

    update server.js

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
+const httpServer = createServer(app);
+const dns = require("dns");
+const io = require("socket.io")(httpServer);
+const { createServer } = require("http");
 const route = require("./routes");
 const dbConfig = require("./configs/dbConfig");
 const app = express();
 dbConfig();
+global.io = io;
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 app.use(express.json());
 app.use(cookieParser());
 app.use(route);
-app.listen(8000, () => {
+httpServer.listen(8000, () => {
   console.log("Server is running");
 });
+
